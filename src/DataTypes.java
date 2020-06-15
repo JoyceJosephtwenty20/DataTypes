44e5fe157f328ae64b23ea9d323228aac287ad38
joycejosephtwenty20 <joyce.j106s13@gmail.com>
2020-06-15 16:31:24 -0400

    data type examples

---
+++ src/DataTypes.java
@@ -0,0 +1,30 @@
+public class DataTypes {
+    public static void main(String[] args){
+        boolean isSkyBlue = true;
+        // These three are not commonly used in our programs.
+        byte myByte = -128;
+        char myCar = 'a';
+        short myShortVariable = -30000;
+        int myInteger = 90000;
+        float myFloat = 7.42f;
+        double myDouble = 90.283333;
+        long longVariable = 9999999;
+
+
+        System.out.println("My boolean variable : "+ isSkyBlue);
+        System.out.println("My byte variable is equal to " + myByte);
+        System.out.println("My car variable is equal to " + myCar);
+        System.out.println("My integer pluse 1k is equal to " + (myInteger + 1000));
+        System.out.println(myFloat);
+
+
+
+
+
+
+    }
+
+
+
+
+}
